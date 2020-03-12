92e929f9b5f43068830cb8240c77bbf837a3adec
circlefintech <circlefintech@circlefintechs-Mac-mini.local>
2020-03-12 14:08:41 +0600

    mutation added

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+// mongoose.connect('mongodb+srv://dev:1234@cluster0-jxtpj.mongodb.net/test?retryWrites=true&w=majority',{
+
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true 
+
+// })
+// mongoose.connection.once('open', () => {
+//     console.log('conneted to database');
+// });
 
 app.use('/graphql', graphqlHTTP({
     schema,
--- schema/schema.js
+++ schema/schema.js
@@ -15,7 +15,7 @@ var books = [
     { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
     { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
     { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '4', authorId: '3' },
     { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
     { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
 ];
@@ -56,6 +56,7 @@ const AuthorType = new GraphQLObjectType({
     })
 });
 
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -90,6 +91,29 @@ const RootQuery = new GraphQLObjectType({
     }
 });
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor:{
+            type: AuthorType,
+            args: {
+                name: { type: GraphQLString },
+                age: { type: GraphQLInt },
+            },
+            resolve(parent, args){
+                let author = {
+                    id: authors.length + 1, 
+                    name: args.name,
+                    age: args.age
+                }
+                 authors.push(author);
+                 return author
+            }
+        }
+    }
+});
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 });
